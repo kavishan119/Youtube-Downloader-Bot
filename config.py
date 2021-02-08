59aa0fcd7ef94b834af633a0a44e747fc7ef85c3
kavishan119 <77564996+kavishan119@users.noreply.github.com>
2021-02-08 17:05:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,10 @@ API_HASH = os.environ.get("API_HASH")
 youtube_next_fetch = 0  # time in minute
 
 
+@bot.on_message(Filters.command('start') & Filters.private)
+async def start(bot, update):
+    await update.reply(
+        f"**Hi {update.chat.first_name} 👋**\n\n"
+        "I am shortlink bot. Just send me link and get adsless short link. \n\nBot Created ❤️ 🇱🇰 by @im_KeKO")
+
 EDIT_TIME = 5
